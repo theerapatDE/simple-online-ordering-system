3c6cbfbbec760a54952bb5a93a03203153c5dfe4
Theerapat.DE <theerapat.hybrid@gmail.com>
2023-09-15 14:02:03 +0700

    create login form and its style

--- login.php
+++ login.php
@@ -1,15 +1,32 @@
-<?php
-session_start();
-?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="login.css">
+    </head>
+    <body>
+        <div class="container login-container">
+            <div class="login-form-1">
+                <h3>Online Material Ordering System</h3>
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Username"><br>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password"><br>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btnSubmit" value="Login">
+                    </div>
+                    <div class="form-group">
+                        <a href="#" class="SignUp">Sign up</a>
+                    </div>
+                </form>
+            </div>         
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
 </html>
