0213708eb9853c270bdfbd5767daafa80800a6ae
Theerapat.DE <theerapat.hybrid@gmail.com>
2023-09-16 21:35:31 +0700

    build product gallery, add products to database, and add cart system

---
+++ cart.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+
+if($_SESSION['login_session'] == "1z2x3c4v"){
+    $customer_id = $_SESSION['customerId'];
+}else{
+    header("location: login.php");
+}
+
+include("connect_db.php");
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Cart</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <!--<link rel="stylesheet" href="login.css">-->
+    </head>
+    <body>
+    <div id="shopping-cart">
+<div class="txt-heading">Shopping Cart</div>
+<a href="index.php">Continue Shopping</a>
+<a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
+<?php
+if(isset($_SESSION["cart_item"])){
+    $total_quantity = 0;
+    $total_price = 0;
+?>	
+<table class="tbl-cart" cellpadding="10" cellspacing="1">
+<tbody>
+<tr>
+<th style="text-align:left;">Name</th>
+<th style="text-align:left;">Code</th>
+<th style="text-align:right;" width="5%">Quantity</th>
+<th style="text-align:right;" width="10%">Unit Price</th>
+<th style="text-align:right;" width="10%">Price</th>
+<th style="text-align:center;" width="5%">Remove</th>
+</tr>	
+<?php		
+    foreach ($_SESSION["cart_item"] as $item){
+        $item_price = $item["quantity"]*$item["price"];
+		?>
+				<tr>
+				<td><?php echo $item["name"]; ?></td>
+				<td><?php echo $item["code"]; ?></td>
+				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+				<td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction">Remove</a></td>
+				</tr>
+				<?php
+				$total_quantity += $item["quantity"];
+				$total_price += ($item["price"]*$item["quantity"]);
+		}
+		?>
+
+<tr>
+<td colspan="2" align="right">Total:</td>
+<td align="right"><?php echo $total_quantity; ?></td>
+<td align="right" colspan="2"><strong><?php echo "$ ".number_format($total_price, 2); ?></strong></td>
+<td></td>
+</tr>
+</tbody>
+</table>		
+  <?php
+} else {
+?>
+<div class="no-records">Your Cart is Empty</div>
+<?php 
+}
+?>
+</div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,198 @@
-<?php echo "Logged in."; ?>
+<?php
+session_start();
+
+if($_SESSION['login_session'] == "1z2x3c4v"){
+    $customer_id = $_SESSION['customerId'];
+}else{
+    header("location: login.php");
+}
+
+include("connect_db.php");
+$page_num = 1;
+if(isset($_GET['page'])){
+    $page_num = $_GET['page'];
+}else{
+    $page_num = 1;
+}
+if(isset($_GET['search'])){
+    $search = $_GET['search'];
+}else{
+    $search = '';
+}
+$results_per_page = 30;  
+$page_first_result = ($page_num-1) * $results_per_page; 
+
+if(isset($_POST['quantity'])){
+    $query = "SELECT * FROM material WHERE MaterialID = ".$_GET['code']. ";"; 
+    $result = $mysqli->query($query);
+    $row = mysqli_fetch_array($result);
+
+    $itemArray = array($row['MaterialID']=>array('name'=>$row['MaterialName'],'code'=>$row['MaterialID'],'quantity'=>$_POST['quantity'],'price'=>$row['Price']));
+
+    if(!empty($_SESSION["cart_item"])) {
+        if(in_array($row['MaterialID'],array_keys($_SESSION["cart_item"]))) {
+            foreach($_SESSION["cart_item"] as $k => $v) {
+                    if($row['MaterialID'] == $k) {
+                        if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+                            $_SESSION["cart_item"][$k]["quantity"] = 0;
+                        }
+                        $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+                    }
+            }
+        } else {
+            $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+        }
+    } else {
+        $_SESSION["cart_item"] = $itemArray;
+    }
+}
+
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Online Material Ordering System</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div id="navbar">
+            <a href="cart.php">Cart</a>
+            <a href="#news"><?=$customer_id?></a>
+            <a href="#contact">Logout</a>
+        </div>
+
+        <div class="container">
+            <div class="row" style="padding-top: 25px;">
+                <div class="col-md-3">
+                    <div class="filter">
+                        <form action="index.php" method="GET">
+                        <div class="form-group">
+                            <input type="text" name="search" class="form-control" placeholder="Material Name" value="<?=$search?>"><br>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="submit" class="btnSubmit" value="Filter">
+                        </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="goods">
+                        <div>
+                            <?php
+                            $query = "SELECT * FROM material";  
+                            if(isset($_GET['search'])){
+                                $query = $query." WHERE MaterialName LIKE '%$search%'";
+                            }
+                            
+                            $result = $mysqli->query($query);
+                            $totalcount = $result->num_rows;
+
+                            if(is_numeric($page_num)){
+                                $page_num = intval($page_num);
+                            }else{
+                                $page_num = 1;
+                            }
+
+                            if($page_num < 1){
+                                $page_num = 1;
+                            }
+
+                            if($totalcount > 30){
+                                $link_num = ceil($totalcount / 30);
+                                if($page_num > $link_num){
+                                    $page_num = $link_num;
+                                }
+                                echo "<h6 style='display:inline'>";
+                                echo "Page : ";
+                                if($page_num != 1){
+                                    echo "<a href='index.php?page=". $page_num-1 ."&search=$search'>Previous</a>";
+                                }
+                                echo " ";
+                                echo "$page_num/$link_num";
+                                echo " ";
+                                if($page_num != $link_num){
+                                    echo "<a href='index.php?page=". $page_num+1 ."&search=$search'>Next</a>";
+                                }
+                                echo "</h6>";
+                            }
+
+                            $query = "SELECT * FROM material";  
+                            if(isset($_GET['search'])){
+                                $query = $query." WHERE MaterialName LIKE '%".$_GET['search']."%'";
+                            }
+                            $query = $query." LIMIT ". $page_first_result . ',' . $results_per_page;
+                            $result2 = $mysqli->query($query);
+                            $count = $result2->num_rows;
+                            $maxrow = ceil($count/3);
+                            echo "<div>";
+                            for($i=0;$i<$maxrow;$i++){
+                                echo "<div class='row' id='itemrow'>";
+                                for($j=0;$j<3;$j++){
+                                    $row = mysqli_fetch_array($result2);
+                                    if(isset($row)){
+                                        echo "<div class='col-md-3' id='item'>";
+                                        echo $row['MaterialName']."<br>";
+                                        echo "Price : ".$row['Price']."฿<br>";
+                                        echo "<form method='POST' action='index.php?page=$page_num&search=$search&action=add&code=". $row['MaterialID'] ."'><input type='number' name='quantity' min='1' style='width:30%'><input type='submit' class='btnSubmit' value='Add to cart'></form>";
+                                        echo "</div>";
+                                    }else{
+                                        exit();
+                                    }
+                                }
+                                echo "</div>"; 
+                            }
+                            echo "</div>"; 
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+        <script>
+            window.onscroll = function() {myFunction()};
+
+            var navbar = document.getElementById("navbar");
+            var sticky = navbar.offsetTop;
+
+            function myFunction() {
+                if (window.pageYOffset >= sticky) {
+                    navbar.classList.add("sticky")
+                } else {
+                    navbar.classList.remove("sticky");
+                }
+            }
+        </script>
+    </body>
+</html>
+
+<?php
+/*case "add":
+	if(!empty($_POST["quantity"])) {
+		$productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
+		$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));
+		
+		if(!empty($_SESSION["cart_item"])) {
+			if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
+				foreach($_SESSION["cart_item"] as $k => $v) {
+						if($productByCode[0]["code"] == $k) {
+							if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+								$_SESSION["cart_item"][$k]["quantity"] = 0;
+							}
+							$_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+						}
+				}
+			} else {
+				$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+			}
+		} else {
+			$_SESSION["cart_item"] = $itemArray;
+		}
+	}
+	break;*/
+?>
